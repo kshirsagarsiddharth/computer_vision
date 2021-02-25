991ad81d6d12012f6487c2061159d5feee3b6cf7
kshirsagarsiddharth <kshirsagarsiddharth45@gmail.com>
2021-02-25 15:24:57 +0530

    model compiling

--- VGG16_dense_conv_train.py
+++ VGG16_dense_conv_train.py
@@ -6,7 +6,12 @@ from tensorflow.keras import models
 from tensorflow.keras import layers 
 from tensorflow.keras.applications import VGG16
 from tensorflow.python.ops.gen_math_ops import mod 
-
+import os
+import numpy as np 
+base_dir = r'D:\project\udemy_cv\Computer-Vision-with-Python\deep_learning\keras_basis.py\cats_and_dogs_small'
+train_dir = os.path.join(base_dir,'train') 
+validation_dir = os.path.join(base_dir,'validation') 
+test_dir = os.path.join(base_dir,'test')
 conv_base = VGG16(weights='imagenet',
                   include_top=False, # wethr to include the densly connected network on top of the network 
                   input_shape=(150,150,3)
@@ -36,4 +41,54 @@ model.summary()
 print("Number of trainable weights before freezing the conv base:", len(model.trainable_weights))
 conv_base.trainable = False 
 print("Number of trainable weights after freezing the conv base:", len(model.trainable_weights))
+
+# with the above setup only weights from the two dense layers will be trained 
+# and these changes should be done before the model is compiled 
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+from tensorflow.keras import optimizers 
+
+train_datagen = ImageDataGenerator(
+    rescale= 1./255, 
+    rotation_range=40, 
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range= 0.2, 
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+test_datagen = ImageDataGenerator( 
+    rescale=1./255
+)
+
+train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=(150,150),
+            batch_size= 20,
+            class_mode='binary'
+)
+
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150,150),
+    batch_size=20,
+    class_mode='binary'
+
+)
+
+
+model = model.compile(loss = 'binary_crossentropy',
+optimizer= optimizers.RMSprop(learning_rate=2e-5),
+metrics = ['accuracy']
+)
+
+
 # %%
+history = model.fit(train_generator,
+steps_per_epoch = 100,
+epochs = 30,
+validation_data = ''
+)
